285035a8300b116c526a8b8a46ab2ee8883648bc
2DIsBetterThan3D <65612005+2DIsBetterThan3D@users.noreply.github.com>
2021-04-28 11:38:03 -0400

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,8 +1,10 @@
 var fquote = [
   'E.... Thats all',
-  'Robux -All roblox n00bs',
-  'PINGAS',
-  'U stinky'
+  "'Robux' -All roblox n00bs",
+  "'PINGAS' -Dr. EGGMAN",
+  "'U stinky' -Unknown",
+  "'YA like jazz'-Bery BEE Benson",
+  "'Do you are is have Robux' -nobbman104g"
 ]
 function newQuote() {
   var randomNumber = Math.floor(Math.random() * (fquote.length));
