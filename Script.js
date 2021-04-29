6fa25879c740b72eab66d6f99dc30e2b3fd9f6d8
2DIsBetterThan3D <65612005+2DIsBetterThan3D@users.noreply.github.com>
2021-04-29 11:10:33 -0400

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,10 +1,13 @@
 var fquote = [
-  'E.... Thats all',
+  "'E.... That's all',
   "'Robux' -All roblox n00bs",
   "'PINGAS' -Dr. EGGMAN",
   "'U stinky' -Unknown",
-  "'YA like jazz'-Bery BEE Benson",
-  "'Do you are is have Robux' -nobbman104g"
+  "'YA like jazz'-Berry BEE Benson",
+  "'Do you are is have Robux' -nobbman104g",
+  "'ZOINKS'-You know who he is, You know",
+  "'Do u r is have stupid'-sdfvajsdfakj, he will not be forgotten...",
+  "'hmmm the floor here seams to be made of floor'-Buzzlightyear"
 ]
 function newQuote() {
   var randomNumber = Math.floor(Math.random() * (fquote.length));
