8d6b8cf72b5428270737bacc430a843722c9c225
2DIsBetterThan3D <65612005+2DIsBetterThan3D@users.noreply.github.com>
2021-04-29 13:22:19 -0400

    Delete meme.js

--- meme.js
+++
@@ -1,8 +0,0 @@
-var fface=[
-     new Image(); img.src = 'https://i.pinimg.com/originals/f4/cd/b0/f4cdb0f288f3f46186a2366cb3fb9b31.jpg'
-           
-]   
-function newFace() {
-  var randomNumber = Math.floor(Math.random() * (fface.length));
-  document.getElementById('funnyFace').innerHTML = fface[randomNumber];
-}
