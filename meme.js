edbb8084e447060b8b910660b40c2be433a87fd1
2DIsBetterThan3D <65612005+2DIsBetterThan3D@users.noreply.github.com>
2021-04-29 13:21:07 -0400

    Update meme.js

--- meme.js
+++ meme.js
@@ -1,6 +1,6 @@
 var fface=[
-     new Image();
-            img.src = 'https://i.pinimg.com/originals/f4/cd/b0/f4cdb0f288f3f46186a2366cb3fb9b31.jpg'
+     new Image(); img.src = 'https://i.pinimg.com/originals/f4/cd/b0/f4cdb0f288f3f46186a2366cb3fb9b31.jpg'
+           
 ]   
 function newFace() {
   var randomNumber = Math.floor(Math.random() * (fface.length));
