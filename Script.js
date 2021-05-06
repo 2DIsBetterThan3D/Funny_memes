10a6781d7262f5b610683af026bd699c6e05d142
Linux_boi_301 <65612005+2DIsBetterThan3D@users.noreply.github.com>
2021-05-06 10:38:42 -0400

    Update Script.js

--- Script.js
+++ Script.js
@@ -34,7 +34,7 @@ randomMeme[4] = "https://wi.wallpapertip.com/wsimgs/156-1565522_puppies-desktop-
 randomMeme[5] = "https://images.unsplash.com/photo-1501265976582-c1e1b0bbaf63?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60";  
   
 
-var number = Math.floor(Math.random()*randomImage.length);   
-return document.getElementById("randomMeme").innerHTML = '<img src="'+randomImage[number]+'" />';  
+var number = Math.floor(Math.random()*randomMeme.length);   
+return document.getElementById("randomMeme").innerHTML = '<img src="'+randomMeme[number]+'" />';  
 }  
 
