54636b298ea8a85440a72efd59deeef0fe4ea22c
Linux_boi_301 <65612005+2DIsBetterThan3D@users.noreply.github.com>
2021-05-06 10:31:34 -0400

    Update Script.js

--- Script.js
+++ Script.js
@@ -22,3 +22,19 @@ function newQuote() {
   var randomNumber = Math.floor(Math.random() * (fquote.length));
   document.getElementById('funnyQuote').innerHTML = fquote[randomNumber];
 }
+function getRandomMeme() {  
+var randomMeme = new Array();  
+  
+
+randomMeme[0] = "https://images.pexels.com/photos/858115/pexels-photo-858115.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500";  
+randomMeme[1] = "http://www.petsworld.in/blog/wp-content/uploads/2014/09/running-cute-puppies.jpg";  
+randomMeme[2] = "https://images.pexels.com/photos/142497/pexels-photo-142497.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500";  
+randomMeme[3] = "https://images.unsplash.com/photo-1543877087-ebf71fde2be1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60";  
+randomMeme[4] = "https://wi.wallpapertip.com/wsimgs/156-1565522_puppies-desktop-wallpaper-desktop-background-puppies.jpg";  
+randomMeme[5] = "https://images.unsplash.com/photo-1501265976582-c1e1b0bbaf63?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60";  
+  
+
+var number = Math.floor(Math.random()*randomImage.length);   
+return document.getElementById("randomMeme").innerHTML = '<img src="'+randomImage[number]+'" />';  
+}  
+
